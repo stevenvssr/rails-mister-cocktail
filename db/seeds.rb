99599b37cab6caa5234e3e6276e6b6003d2160cc
Steven Visser <steven_v1987@live.nl>
2020-11-20 17:27:45 +0100

    fssfd

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,6 +2,9 @@ require 'json'
 require 'open-uri'
 require 'nokogiri'
 
+Ingredient.destroy_all
+Cocktail.destroy_all
+
 result = JSON.parse(open('http://www.thecocktaildb.com/api/json/v1/1/list.php?i=list').read)
 drinks = result['drinks']
 drinks.each do |hash|
@@ -10,23 +13,29 @@ drinks.each do |hash|
   end
 end
 
-    # html_content = 'https://www.cocktailicious.nl/index-cocktails/'
-    # cocktail_titles = []
-    # html_file = open(html_content).read
-    # html_doc = Nokogiri::HTML(html_file)
-    # html_doc.search('.elementor-post').each do |element|
-    #   name = element.search(".elementor-post__title").text.strip
-    #   picture_path = element.search(".elementor-post__thumbnail__link img").attr("src").value
-    #   p picture_path
-    #   # p name
-    #   # p picture_path.doc.attr('src').value
-    #   # p picture_path.xpath('//div/@src')
+html_content = 'https://www.cocktailicious.nl/index-cocktails/'
+# cocktail_titles = []
+html_file = open(html_content).read
+html_doc = Nokogiri::HTML(html_file)
+html_doc.search('.elementor-post').each do |element|
+  name = element.search(".elementor-post__title").text.strip
+  picture_path = element.search(".elementor-post__thumbnail__link img").attr("src").value
+  file = URI.open(picture_path)
+  cocktail = Cocktail.new(name: name)
+  cocktail.photo.attach(io: file, filename: "#{cocktail.name}.png", content_type: 'image/png')
+  cocktail.save
 
-    #   # image_array = []
-    #   # html_doc.search('.attachment-medium').each do |element|
-    #   #   image_array << element.attr('src')
-    #   #   new_array = image_array.each_with_index.map{|url, index| url if index % 2 == 0}
-    #   # end
-    #   # p image_array
-    #   Cocktail.create(name: element.text.strip)
-    # end
+  p cocktail.name
+end
+
+
+puts "#{Ingredient.count} ingredients created"
+puts "#{Cocktail.count} cocktails created"
+
+
+      # image_array = []
+      # html_doc.search('.attachment-medium').each do |element|
+      #   image_array << element.attr('src')
+      #   new_array = image_array.each_with_index.map{|url, index| url if index % 2 == 0}
+      # end
+      # p image_array
