6b39de7aae062132b9c99313895b279da27abf2f
Steven Visser <steven_v1987@live.nl>
2020-11-20 15:57:20 +0100

    Add dotenv - Protect my secret data in .env file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,6 @@
 require 'json'
 require 'open-uri'
+require 'nokogiri'
 
 result = JSON.parse(open('http://www.thecocktaildb.com/api/json/v1/1/list.php?i=list').read)
 drinks = result['drinks']
@@ -7,4 +8,25 @@ drinks.each do |hash|
   hash.each do |k, v|
     Ingredient.create!(name: v)
   end
+end
 
+    html_content = 'https://www.cocktailicious.nl/index-cocktails/'
+    cocktail_titles = []
+    html_file = open(html_content).read
+    html_doc = Nokogiri::HTML(html_file)
+    html_doc.search('.elementor-post').each do |element|
+      name = element.search(".elementor-post__title").text.strip
+      picture_path = element.search(".elementor-post__thumbnail__link img").attr("src").value
+      p picture_path
+      # p name
+      # p picture_path.doc.attr('src').value
+      # p picture_path.xpath('//div/@src')
+
+      # image_array = []
+      # html_doc.search('.attachment-medium').each do |element|
+      #   image_array << element.attr('src')
+      #   new_array = image_array.each_with_index.map{|url, index| url if index % 2 == 0}
+      # end
+      # p image_array
+      Cocktail.create(name: element.text.strip)
+    end
